5f8cbcc62398ef546ada959e7f483d3f83c435b0
Nemo <63228128+xNEMOx@users.noreply.github.com>
2021-12-07 13:00:24 +0400

    Add files via upload

---
+++ storing-input.py
@@ -0,0 +1,13 @@
+#Getting Where To Save File
+where = ('/Users/neymatgasimli/Desktop/Python-BlockChain/storage.txt')
+
+#Getting What To Write To File
+login = input("Login: ")
+passz = input("Password: ")
+
+#Actually Writing It
+saveFile = open(where, 'w')
+saveFile.write("login: " + login + "            " +"Password: " + passz)
+saveFile.close()
+
+# python3 inputtest.py
